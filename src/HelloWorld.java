1271982571394631d589fef5820a48858282a02c
chpranusha <chpranusha90@gmail.com>
2023-01-02 11:02:46 -0500

    first commit of helloworld to central repo

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+//This class will print hello world!
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,Pranusha!");
+
+	}//End of the main method
+
+}//End of class
+/* end of class
+gsdhgdhjfjd
+*/
